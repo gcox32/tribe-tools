0217916330be99a4931bdc57d7b4a137b7b45baf
Grant Cox <gcoxexcel@gmail.com>
2023-07-16 14:14:21 -0400

    swapped out a chart

--- src/pages/DashboardAppPage.jsx
+++ src/pages/DashboardAppPage.jsx
@@ -12,7 +12,6 @@ import {
   AppWebsiteVisits,
   AppWidgetSummary,
   AppCurrentSubject,
-  AppConversionRates,
 } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
@@ -104,25 +103,6 @@ export default function DashboardAppPage() {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Franchise Breakdown"
-              subheader="Generic Subheader"
-              chartData={[
-                { label: 'Homewood', value: 400 },
-                { label: 'Brentwood', value: 430 },
-                { label: 'NOLA', value: 448 },
-                { label: 'Highway 280', value: 470 },
-                { label: 'Germantown', value: 540 },
-                { label: 'Raleigh', value: 580 },
-                { label: 'East Memphis', value: 690 },
-                { label: 'Charleston', value: 1100 },
-                { label: 'Richmond', value: 1200 },
-                { label: 'Tampa', value: 1380 },
-              ]}
-            />
-          </Grid>
-
           <Grid item xs={12} md={6} lg={4}>
             <AppCurrentSubject
               title="Strengths Profile"
@@ -133,6 +113,46 @@ export default function DashboardAppPage() {
                 { name: 'ITF', data: [44, 76, 78, 13, 43, 10] },
               ]}
               chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={8}>
+            <AppWebsiteVisits
+              title="Body Composition"
+              subheader="Generic Subheader"
+              chartLabels={[
+                '01/01/2022',
+                '02/01/2022',
+                '03/01/2022',
+                '04/01/2022',
+                '05/01/2022',
+                '06/01/2022',
+                '07/01/2022',
+                '08/01/2022',
+                '09/01/2022',
+                '10/01/2022',
+                '11/01/2022',
+              ]}
+              chartData={[
+                {
+                  name: 'Body Weight (lbs)',
+                  type: 'line',
+                  fill: 'solid',
+                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                },
+                {
+                  name: 'Body Fat (%)',
+                  type: 'line',
+                  fill: 'solid',
+                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                },
+                {
+                  name: 'Muscle Mass (lbs)',
+                  type: 'line',
+                  fill: 'solid',
+                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                },
+              ]}
             />
           </Grid>
 
@@ -155,54 +175,6 @@ export default function DashboardAppPage() {
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="Upcoming Events"
-              subheader="These are all the same because fake data is tedious"
-              list={
-                [
-                  {
-                    'id':'00001',
-                    'title':'Workout for Water',
-                    'description':'Hosted at ITF East Memphis',
-                    'image':`/assets/images/covers/cover_1.jpg`,
-                    'postedAt':faker.date.past(),
-                  },
-                  {
-                    'id':'00002',
-                    'title':'Workout for Water',
-                    'description':'Hosted at ITF East Memphis',
-                    'image':`/assets/images/covers/cover_2.jpg`,
-                    'postedAt':faker.date.recent(),
-                  },
-                  {
-                    'id':'00003',
-                    'title':'Workout for Water',
-                    'description':'Hosted at ITF East Memphis',
-                    'image':`/assets/images/covers/cover_3.jpg`,
-                    'postedAt':faker.date.soon(),
-                  },
-                  {
-                    'id':'00004',
-                    'title':'Workout for Water',
-                    'description':'Hosted at ITF East Memphis',
-                    'image':`/assets/images/covers/cover_4.jpg`,
-                    'postedAt':faker.date.future(),
-                  },
-                  {
-                    'id':'00005',
-                    'title':'Workout for Water',
-                    'description':'Hosted at ITF East Memphis',
-                    'image':`/assets/images/covers/cover_5.jpg`,
-                    'postedAt':faker.date.future(),
-                  },
-                ]
-              }
-            />
-          </Grid>
-
-
-
-          <Grid item xs={12} md={6} lg={8}>
             <AppTasks
               title="Goal Tracker"
               list={[
@@ -214,7 +186,54 @@ export default function DashboardAppPage() {
               ]}
             />
           </Grid>
+
+          <Grid item xs={12} md={12} lg={12}>
+            <AppNewsUpdate
+              title="Upcoming Events"
+              subheader="These are all the same because fake data is tedious"
+              list={
+                [
+                  {
+                    'id': '00001',
+                    'title': 'Workout for Water',
+                    'description': 'Hosted at ITF East Memphis',
+                    'image': `/assets/images/covers/cover_1.jpg`,
+                    'postedAt': faker.date.past(),
+                  },
+                  {
+                    'id': '00002',
+                    'title': 'Workout for Water',
+                    'description': 'Hosted at ITF East Memphis',
+                    'image': `/assets/images/covers/cover_2.jpg`,
+                    'postedAt': faker.date.recent(),
+                  },
+                  {
+                    'id': '00003',
+                    'title': 'Workout for Water',
+                    'description': 'Hosted at ITF East Memphis',
+                    'image': `/assets/images/covers/cover_3.jpg`,
+                    'postedAt': faker.date.soon(),
+                  },
+                  {
+                    'id': '00004',
+                    'title': 'Workout for Water',
+                    'description': 'Hosted at ITF East Memphis',
+                    'image': `/assets/images/covers/cover_4.jpg`,
+                    'postedAt': faker.date.future(),
+                  },
+                  {
+                    'id': '00005',
+                    'title': 'Workout for Water',
+                    'description': 'Hosted at ITF East Memphis',
+                    'image': `/assets/images/covers/cover_5.jpg`,
+                    'postedAt': faker.date.future(),
+                  },
+                ]
+              }
+            />
+          </Grid>
         </Grid>
+
       </Container>
     </>
   );
